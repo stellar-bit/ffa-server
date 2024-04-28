4e30a3e565bb4596941ecee9a65a01721f947d48
Patrik Cihal <patrik.cihal1234@gmail.com>
2024-04-28 12:20:40 +0200

    Renaming

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ pub use hub_connection::ServerHubConn;
 
 
 pub const SERVER_ADDR: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(213, 211, 62, 122), 39453);
-pub const SERVER_NAME: &str = "Free for all";
+pub const SERVER_NAME: &str = "Official FFA";
 
 pub async fn start_server(hub_conn: ServerHubConn) {
     let server_state = Arc::new(RwLock::new(ServerState::default()));
